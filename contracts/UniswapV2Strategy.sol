7daada6321ec3c454b74516f44bc4a87dbe567ee
Lucas <lucas@maple.finance>
2021-10-04 14:41:55 -0400

    fix: remove unused submodules

--- contracts/UniswapV2Strategy.sol
+++ contracts/UniswapV2Strategy.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.7;
 
-import { ILoan }       from "../modules/loan/contracts/interfaces/ILoan.sol";
 import { ERC20Helper } from "../modules/erc20-helper/src/ERC20Helper.sol";
 import { IERC20 }      from "../modules/erc20-helper/lib/erc20/src/interfaces/IERC20.sol";
 
