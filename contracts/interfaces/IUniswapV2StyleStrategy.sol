09400f6203f0b1b6edb699b87ec43b2729f588c5
JG Carvalho <jg@jgcarv.com>
2021-12-10 16:54:37 -0300

    Feat: improve strategy and cleanup

--- contracts/interfaces/IUniswapV2StyleStrategy.sol
+++ contracts/interfaces/IUniswapV2StyleStrategy.sol
@@ -4,47 +4,46 @@ pragma solidity 0.8.7;
 interface IUniswapV2StyleStrategy {
 
     /**
-     * @dev View function that returns the router that is used by the strategy
+     * @dev View function that returns the router that is used by the strategy.
      */
     function ROUTER() external view returns (address router_);
 
     /**
-     * @dev Function that performs a `swapExactTokensForTokens` swap on a UniswapV2-style AMM, sending the remaining funds
-     * @dev from the flash loan to the specified `profitDestination`.
-     * @param swapAmount_         Amount of `collateralAsset_` to be swapped.
-     * @param minReturnAmount_    Minimum amount of `fundsAsset_` to be returned from the swap.
-     * @param collateralAsset_    Asset that is swapped from.
-     * @param middleAsset_        Optional middle asset to add to `path` of the AMM.
-     * @param fundsAsset_         Asset to be swapped to.
-     * @param profitDestination_  Address that remaining funds are sent to.
+     * @notice Function that calls `liquidatePortion` in the liquidator, flash-borrowing funds to swap.
+     * @param  flashLender_        Address that will flash loan `collateralBorrowed_` of `collateralAsset`
+     * @param  collateralBorrowed_ Amount of `collateralAsset_` to be swapped.
+     * @param  maxReturnFunds_     Max amount of `fundsAsset` that can be returned to the liquidator contract.
+     * @param  minFundsProfit_     Min profit in funds.
+     * @param  collateralAsset_    Asset that is flash-borrowed.
+     * @param  middleAsset_        Optional middle asset to add to `path` of the AMM.
+     * @param  fundsAsset_         Asset to be swapped to.
+     * @param  destination_        Address that remaining funds and collateral are sent to.
      */
-    function swap(
-        uint256 swapAmount_,
-        uint256 minReturnAmount_,
+    function flashBorrowLiquidation(
+        address flashLender_,
+        uint256 collateralBorrowed_,
+        uint256 maxReturnFunds_,
+        uint256 minFundsProfit_,
         address collateralAsset_,
         address middleAsset_,
         address fundsAsset_,
-        address profitDestination_
+        address destination_
     ) external;
 
     /**
-     * @dev Function that calls `liquidatePortion` in the liquidator, flash-borrowing funds to swap.
-     * @param lender_             Address that will flash loan `swapAmount_` of `collateralAsset`
-     * @param swapAmount_         Amount of `collateralAsset_` to be swapped.
-     * @param maxReturnAmount_    Max amount of `fundsAsset` that can be returned to the liquidator contract.
-     * @param collateralAsset_    Asset that is flash-borrowed.
-     * @param middleAsset_        Optional middle asset to add to `path` of the AMM.
-     * @param fundsAsset_         Asset to be swapped to.
-     * @param profitDestination_  Address that remaining funds are sent to.
+     * @notice Function that performs a `swapExactTokensForTokens` swap on a UniswapV2-style AMM, sending the remaining funds
+     *         from the flash loan to the specified `profitDestination`.
+     * @param  collateralBorrowed_ Amount of `collateralAsset_` to be swapped.
+     * @param  minFundsOut_        Minimum amount of `fundsAsset_` to be returned from the swap.
+     * @param  collateralAsset_    Asset that is swapped from.
+     * @param  middleAsset_        Optional middle asset to add to `path` of the AMM.
+     * @param  fundsAsset_         Asset to be swapped to.
      */
-    function flashBorrowLiquidation(
-        address lender_,
-        uint256 swapAmount_,
-        uint256 maxReturnAmount_,
+    function swap(
+        uint256 collateralBorrowed_,
+        uint256 minFundsOut_,
         address collateralAsset_,
         address middleAsset_,
-        address fundsAsset_,
-        address profitDestination_
+        address fundsAsset_
     ) external;
 }
-
