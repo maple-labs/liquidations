c41dc5dfb4571dae14e0b4dca4e3a3113626cada
Lucas Manuel <lucas@maple.finance>
2021-12-12 12:03:12 -0500

    fix update name

--- contracts/UniswapV2Strategy.sol
+++ contracts/UniswapV2Strategy.sol
@@ -6,7 +6,7 @@ import { ERC20Helper } from "../modules/erc20-helper/src/ERC20Helper.sol";
 import { IERC20Like, ILiquidatorLike, IUniswapRouterLike } from "./interfaces/Interfaces.sol";
 import { IUniswapV2StyleStrategy }                         from "./interfaces/IUniswapV2StyleStrategy.sol";
 
-contract UV2S is IUniswapV2StyleStrategy {
+contract UniswapV2Strategy is IUniswapV2StyleStrategy {
 
     address public constant override ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
