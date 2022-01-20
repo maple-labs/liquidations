4d1dc59ad026cde20d1e270bd46d80d4c6c05e7f
Lucas Manuel <lucas@maple.finance>
2022-01-19 22:37:36 -0500

    chore: use flags, match to make output smaller

--- test.sh
+++ test.sh
@@ -13,12 +13,12 @@ runs=$([ -z "$runs" ] && echo "1" || echo "$runs")
 
 [[ $SKIP_MAINNET_CHECK || "$ETH_RPC_URL" && "$(seth chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
 
-export DAPP_TEST_TIMESTAMP=1633366990
-export DAPP_TEST_NUMBER=13353817
+# export DAPP_TEST_TIMESTAMP=1633366990
+# export DAPP_TEST_NUMBER=13353817
 export DAPP_SOLC_VERSION=0.8.7
 export DAPP_SRC="contracts"
 export PROPTEST_CASES=$runs
 
 if [ -z "$test" ]; then match="[contracts/test/*.t.sol]"; else match=$test; fi
 
-forge test --match "$match" --rpc-url "$ETH_RPC_URL" --lib-paths "modules" -vv --optimize ##--block-timestamp $DAPP_TEST_TIMESTAMP --block-number $DAPP_TEST_NUMBER
+../foundry/target/debug/forge test --match "$match" --rpc-url "$ETH_RPC_URL" --lib-paths "modules" -vv --optimize --block-timestamp 1633366990 --block-number 13353817
