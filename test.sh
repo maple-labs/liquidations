9a5477300d7fe147297f4ffd66537066f46fd4a1
Lucas Manuel <lucas@maple.finance>
2022-01-24 14:01:45 -0500

    ci: Refactor to forge (SC-4615) (#33)
    * feat: set up dapp/forge comparison, timestamp/number not working
    
    * chore: use flags, match to make output smaller
    
    * chore: update test scripts to work with forge
    
    * ci: add gh workflow
    
    * ci: setup ci
    
    * feat: use secret
    
    * fix: use cast
    
    * fix: rm debug events and add newlines
    
    * fix: rm old branch
    
    * fix: update title
    
    * fix: update naming in CI, reduce fuzz runs

--- test.sh
+++ test.sh
@@ -1,35 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-while getopts t:r:b:v:c: flag
+while getopts t:r: flag
 do
     case "${flag}" in
         t) test=${OPTARG};;
         r) runs=${OPTARG};;
-        b) build=${OPTARG};;
-        c) config=${OPTARG};;
     esac
 done
 
 runs=$([ -z "$runs" ] && echo "1" || echo "$runs")
-build=$([ -z "$build" ] && echo "1" || echo "$build")
-config=$([ -z "$config" ] && echo "./config/dev.json" || echo "$config")
-skip_build=$([ "$build" == "0" ] && echo "1" || echo "0")
 
-[[ $SKIP_MAINNET_CHECK || "$ETH_RPC_URL" && "$(seth chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
+[[ $SKIP_MAINNET_CHECK || "$ETH_RPC_URL" && "$(cast chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
 
-export DAPP_TEST_TIMESTAMP=1633366990
-export DAPP_TEST_NUMBER=13353817
+export DAPP_FORK_BLOCK=13353817
 export DAPP_SOLC_VERSION=0.8.7
 export DAPP_SRC="contracts"
-export DAPP_LINK_TEST_LIBRARIES=0
-export DAPP_STANDARD_JSON=$config
-export DAPP_TEST_SMTTIMEOUT=360000
+export PROPTEST_CASES=$runs
 
-if [ "$skip_build" = "1" ]; then export DAPP_SKIP_BUILD=1; fi
+if [ -z "$test" ]; then match="[contracts/test/*.t.sol]"; else match=$test; fi
 
-if [ -z "$test" ]; then match="[contracts/test/*.t.sol]"; dapp_test_verbosity=1; else match=$test; dapp_test_verbosity=2; fi
-
-echo LANG=C.UTF-8 dapp test --match "$match" --rpc-url "$ETH_RPC_URL" --verbosity $dapp_test_verbosity --fuzz-runs $runs
-
-LANG=C.UTF-8 dapp test --match "$match" --rpc-url "$ETH_RPC_URL" --verbosity $dapp_test_verbosity --fuzz-runs $runs
+forge test --match "$match" --rpc-url "$ETH_RPC_URL" --lib-paths "modules" -vv --optimize
